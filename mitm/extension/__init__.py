9223ae1a0b3239852e9e4d4dd623bcbc8ec3517e
synchronizing <2829082+synchronizing@users.noreply.github.com>
2022-06-13 15:44:45 -0400

    Improved imports.

--- mitm/extension/__init__.py
+++ mitm/extension/__init__.py
@@ -1,5 +1,7 @@
 """
 Built-in extensions for mitm.
 """
+from mitm.extension import middleware
+from mitm.extension import protocol
 from mitm.extension.middleware import *
 from mitm.extension.protocol import *
